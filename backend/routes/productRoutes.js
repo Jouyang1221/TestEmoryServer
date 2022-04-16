581902c0e0193163b659c87772190212700371bf
Jouyang1221 <jouyang1221@gmail.com>
2022-04-16 18:17:47 -0400

    Post listing without the user, username. image
    the post listing sends an item to the database, but i have not incoporated the users, images, username attributes yet

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Product from "../models/productModel.js";
 const router = express.Router();
 import {
   getProducts,
@@ -10,5 +11,15 @@ router.route("/:id").get(getProductById);
 
 // router.get("/", getProducts);
 // router.get("/:id", getProductById);
+
+router.post("/", async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProducts = await newProduct.save();
+    res.status(200).json(savedProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 export default router;
